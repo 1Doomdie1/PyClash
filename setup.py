445a7576eb9dd1db0d2554c073fa6d37a7460d20
1Doomdie1 <horia.todoran@yahoo.com>
2025-07-10 21:30:06 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as f:
+    req_list = f.read().split("\n")[:-1]
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name                          = "PyClash",
+    version                       = "0.0.1",
+    description                   = "API Wrapper for Clash of Clans",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "https://github.com/1Doomdie1/PyClash",
+    author                        = "Todoran Horia",
+    license                       = "GPL-3.0",
+    classifiers                   = [
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires              = req_list,
+    extras_require                = {
+        "dev": ["pytest", "twine"],
+    },
+    python_requires               = ">=3.10"
+)
